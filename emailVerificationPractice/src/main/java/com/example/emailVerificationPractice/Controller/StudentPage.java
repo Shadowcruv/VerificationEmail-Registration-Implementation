bf1ba7e81c207d07ebef80d06ca5219f7107f750
miracleihediwa <miracleihediwa619@gmail.com>
2022-11-04 12:52:04 +0100

    crossorigin

--- emailVerificationPractice/src/main/java/com/example/emailVerificationPractice/Controller/StudentPage.java
+++ emailVerificationPractice/src/main/java/com/example/emailVerificationPractice/Controller/StudentPage.java
@@ -4,11 +4,9 @@ import com.example.emailVerificationPractice.Entity.ApiUser;
 import com.example.emailVerificationPractice.Service.ApiStudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/studentspage")
 @RestController
 public class StudentPage {
