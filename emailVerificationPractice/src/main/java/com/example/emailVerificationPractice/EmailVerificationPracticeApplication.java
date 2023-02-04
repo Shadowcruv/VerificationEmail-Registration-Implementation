eb54c422f16c8d61c3b402328fa6b22ad1d8dc35
miracleihediwa <miracleihediwa619@gmail.com>
2023-02-04 18:23:33 +0100

    first commit

--- emailVerificationPractice/src/main/java/com/example/emailVerificationPractice/EmailVerificationPracticeApplication.java
+++ emailVerificationPractice/src/main/java/com/example/emailVerificationPractice/EmailVerificationPracticeApplication.java
@@ -10,4 +10,14 @@ public class EmailVerificationPracticeApplication {
 		SpringApplication.run(EmailVerificationPracticeApplication.class, args);
 	}
 
+	/*
+	spring.datasource.url = jdbc:postgresql://ec2-44-199-9-102.compute-1.amazonaws.com:5432/d2i525hhm0vvko
+spring.datasource.username= xdxtwjqpxhevbe
+spring.datasource.password= 57a85fcc45c511376f19f213e54b7bd580aa450a5d83aa5d6bb615ad4cb34045
+spring.jpa.hibernate.ddl-auto=update
+spring.jpa.show-sql=true
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect
+spring.jpa.properties.hibernate.format_sql=true
+	 */
+
 }
